cc3bbf960d3815a67c48afda51863f91a3009d9c
enan <hant2258@gmail.com>
2021-03-20 02:00:10 +0900

    이것이 코딩테스트다 5-이진 탐색

--- 이것이 코딩테스트다/이진 탐색/가사 검색.py
+++ 이것이 코딩테스트다/이진 탐색/가사 검색.py
@@ -1,29 +1,49 @@
-from bisect import bisect_right, bisect_left
 
+class Node:
+    def __init__(self, val, cnt=0):
+        self.val = val
+        self.cnt = cnt
+        self.child = {}
+
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        cur = self.head
+        cur.cnt += 1
+        for ch in string:
+            if ch not in cur.child:
+                cur.child[ch] = Node(ch)
+            cur = cur.child[ch]
+            cur.cnt += 1
+
+    def search(self, string):
+        cur = self.head
+        for ch in string:
+            if ch == '?':
+                break
+            elif ch not in cur.child:
+                return 0
+            cur = cur.child[ch]
+        return cur.cnt
 
 def solution(words, queries):
     answer = []
-    words.sort()
-    reversed_words = []
+    trie = [Trie() for _ in range(10001)]
+    eirt = [Trie() for _ in range(10001)]
     for word in words:
-        reversed_words.append(word[::-1])
-    cwords = []
+        trie[len(word)].insert(word)
+        eirt[len(word)].insert(word[::-1])
     for query in queries:
-        queryz = ""
         if query[0] == '?':
-            query = query[::-1]
-            cwords = reversed_words
-        for i in range(len(query)):
-            if query[i] == '?':
-                queryz += 'z'
-            else:
-                queryz += query[i]
-        left = bisect_left(cwords, query)
-        right = bisect_right(cwords, queryz)
-        count = 0
-        for i in range(left, right):
-            if len(cwords[i]) == len(query):
-                count += 1
-
-        answer.append(count)
+            answer.append(eirt[len(query)].search(query[::-1]))
+        else:
+            answer.append(trie[len(query)].search(query))
     return answer
+
+words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+queries = ["fro??", "????o", "fr???", "fro???", "pro?","?????"]
+
+print(solution(words,queries))
